f800aafa02e3728d4495cb0a9b46a56a5e726e54
MarinaTrdin <mana2411@gmail.com>
2024-05-26 01:22:16 +0200

    ERAracun

---
+++ ERAracun.sql
@@ -0,0 +1,24 @@
+--create database ERAracun;
+
+--drop database ERAracun;
+
+--use edunovawp5;
+
+--use fakultet;
+--use ERAracun;
+
+--create table racun(
+--sifra int,
+--rednibroj int,
+--datum datetime,
+--kupac varchar(50),
+--placeno decimal(18,2)
+--);
+
+--create table StavkeRacuna(
+--racun int,
+--artikl varchar(50),
+--cijena decimal(18,2),
+--kolicina int,
+--popust int
+--);
